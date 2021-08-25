9962cbf6007e78efaace7a433a24e7149372bd84
Brent <brent@dxw.com>
2021-08-25 13:51:19 +0100

    Initial setup
    - Main plugin file and package naming update
    - Composer setup

---
+++ dxw-media-controls.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * dxw Media controls
+ *
+ * @package     dxwMediaControls
+ * @author      dxw
+ * @copyright   2020
+ * @license     MIT
+ *
+ * @dxw-media-controls
+ * Plugin Name: dxw Media controls
+ * Plugin URI: https://github.com/dxw/dxw-media-controls
+ * Description: Manage media uploading: file sizes and types with user guidance
+ * Author: dxw
+ * Version: 0.1.0
+ * Network: True
+ */
+
+$registrar = require __DIR__.'/src/load.php';
+$registrar->register();
