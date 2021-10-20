476d9e47fae34a24dcd9a1fc0ad1e06928a64f4b
Brent <brent@dxw.com>
2021-10-20 10:25:07 +0100

    Fix linting
    - Resolves: https://dxw.zendesk.com/agent/tickets/13732

--- src/Guidance.php
+++ src/Guidance.php
@@ -7,14 +7,17 @@ class Guidance implements \Dxw\Iguana\Registerable
     {
         if (get_field('dxw_media_show_guidance', 'option') && get_field('dxw_media_controls_guidance', 'option')) {
             add_action('pre-upload-ui', [$this, 'media_guidance']);
-            add_action( 'enqueue_block_editor_assets', function() {
-                wp_register_script('dxw-media-controls-script',
-                    plugins_url( 'assets/js/script.js', __FILE__ )
+            add_action('enqueue_block_editor_assets', function () {
+                wp_register_script(
+                    'dxw-media-controls-script',
+                    plugins_url('assets/js/script.js', __FILE__)
                 );
-                wp_localize_script( 'dxw-media-controls-script', 'dxwMediaControls',
-                    array(
+                wp_localize_script(
+                    'dxw-media-controls-script',
+                    'dxwMediaControls',
+                    [
                         'dxwMediaGuidance' => get_field('dxw_media_controls_guidance', 'option')
-                    )
+                    ]
                 );
                 wp_enqueue_script('dxw-media-controls-script');
             });
