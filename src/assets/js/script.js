82fb2e32d63134d23c789472c0c68769a2034510
Brent <brent@dxw.com>
2021-10-20 10:22:28 +0100

    JavaScript to add user media guidance to media block on block editor
    - Resolves: https://dxw.zendesk.com/agent/tickets/13732

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,3 +1,17 @@
 jQuery(function() {
-    console.log("dxw-media-controls-script")
+
+    // block editor: on change check if media block opened and add user guidance if not already added
+    wp.data.subscribe( () => {
+        let mediaBlock = jQuery('.wp-block-image').first();
+        let dxwMediaUserGuidanceDiv = jQuery('.dxw-media-user-guidance').first();
+        let dxwMediaUserGuidance = dxwMediaUserGuidanceDiv.length > 0;
+        if (mediaBlock && !dxwMediaUserGuidance) {
+            let instructionsDiv = jQuery('.components-placeholder__instructions').first();
+            if (instructionsDiv) {
+                let html = '<div class="dxw-media-user-guidance">' + dxwMediaControls.dxwMediaGuidance +'</div>';
+                jQuery(instructionsDiv).append(html);
+            }
+        }
+    });
+
 });
