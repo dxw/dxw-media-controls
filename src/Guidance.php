e5de86d4f4db0e99f99b60c4c8e1c43c0af607c1
Rob Skilling <rob@dxw.com>
2024-12-11 10:34:24 +0000

    Apply PHP Linter fixes

--- src/Guidance.php
+++ src/Guidance.php
@@ -1,33 +1,34 @@
 <?php
+
 namespace dxwMediaControls;
 
 class Guidance implements \Dxw\Iguana\Registerable
 {
-    public function register() : void
-    {
-        if (get_field('dxw_media_show_guidance', 'option') && get_field('dxw_media_controls_guidance', 'option')) {
-            add_action('pre-upload-ui', [$this, 'media_guidance']);
-            add_action('enqueue_block_editor_assets', function () {
-                wp_register_script(
-                    'dxw-media-controls-script',
-                    plugins_url('assets/js/script.js', __FILE__)
-                );
-                wp_localize_script(
-                    'dxw-media-controls-script',
-                    'dxwMediaControls',
-                    [
-                        'dxwMediaGuidance' => get_field('dxw_media_controls_guidance', 'option')
-                    ]
-                );
-                wp_enqueue_script('dxw-media-controls-script');
-            });
-        }
-    }
+	public function register(): void
+	{
+		if (get_field('dxw_media_show_guidance', 'option') && get_field('dxw_media_controls_guidance', 'option')) {
+			add_action('pre-upload-ui', [$this, 'media_guidance']);
+			add_action('enqueue_block_editor_assets', function () {
+				wp_register_script(
+					'dxw-media-controls-script',
+					plugins_url('assets/js/script.js', __FILE__)
+				);
+				wp_localize_script(
+					'dxw-media-controls-script',
+					'dxwMediaControls',
+					[
+						'dxwMediaGuidance' => get_field('dxw_media_controls_guidance', 'option')
+					]
+				);
+				wp_enqueue_script('dxw-media-controls-script');
+			});
+		}
+	}
 
-    public function media_guidance() : void
-    {
-        echo '<h4>';
-        echo get_field('dxw_media_controls_guidance', 'option');
-        echo '</h4>';
-    }
+	public function media_guidance(): void
+	{
+		echo '<h4>';
+		echo get_field('dxw_media_controls_guidance', 'option');
+		echo '</h4>';
+	}
 }
